5f479f2b93b697717eb5b18e2c76a752979e286e
Park Ye Eun <komgolo67@naver.com>
2024-01-16 22:48:39 +0900

    ✨[feat] : 로그인 테이블 생성 및 post api 기능 추가

--- src/main/java/com/wevel/wevel_server/controller/UsersController.java
+++ src/main/java/com/wevel/wevel_server/controller/UsersController.java
@@ -1,4 +1,35 @@
 package com.wevel.wevel_server.controller;
+import com.wevel.wevel_server.dto.ApiResponse;
+import com.wevel.wevel_server.entity.Users;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+import com.wevel.wevel_server.dto.UsersDTO;
+import com.wevel.wevel_server.service.UsersService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
 public class UsersController {
+
+    private final UsersService usersService;
+
+    public UsersController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createUser(@RequestBody UsersDTO usersDTO) {
+        try {
+            usersService.createUser(usersDTO);
+            return new ResponseEntity<>(new ApiResponse("유저 테이블에 추가되었습니다."), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ApiResponse("유저 추가가 안되었습니다."), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // 다른 API 엔드포인트 및 메소드들을 추가할 수 있습니다.
 }
---
+++ src/main/java/com/wevel/wevel_server/dto/ApiResponse.java
@@ -0,0 +1,12 @@
+package com.wevel.wevel_server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ApiResponse {
+    private String message;
+}
