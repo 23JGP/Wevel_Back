49ced7fed82f874a369183cc4345d7440d523e65
mic050r <103114387+mic050r@users.noreply.github.com>
2024-01-17 19:42:07 +0900

    ✨[feat] : Social 인증 커스텀 설정하기

--- src/main/java/com/wevel/wevel_server/config/SecurityConfig.java
+++ src/main/java/com/wevel/wevel_server/config/SecurityConfig.java
@@ -1,16 +1,34 @@
 package com.wevel.wevel_server.config;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
+    private final Environment environment;
+    private final String registration = "spring.security.oauth2.client.registration.";
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
@@ -18,6 +36,50 @@ public class SecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFil
                         .requestMatchers("/login", "/index").permitAll()
                         .anyRequest().authenticated()
                 )
-                .oauth2Login(Customizer.withDefaults());
+                .oauth2Login(oauth2 -> oauth2
+                        .clientRegistrationRepository(clientRegistrationRepository())
+                        .authorizedClientService(auth2AuthorizedClientService())
+                );
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientService auth2AuthorizedClientService() {
+        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository());
+    }
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        final List<ClientRegistration> clientRegistrations = Arrays.asList(
+                googleClientRegistration(),
+                facebookClientRegistration()
+        );
+
+        return new InMemoryClientRegistrationRepository(clientRegistrations);
+    }
+
+    private ClientRegistration googleClientRegistration() {
+        // yml에 있는 id, secret 가져오기
+        final String clientId = environment.getProperty(registration + "google.client-id");
+        final String clientSecret = environment.getProperty(registration + "google.client-secret");
+
+        return CommonOAuth2Provider
+                .GOOGLE
+                .getBuilder("google")
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .build();
+    }
+
+
+    private ClientRegistration facebookClientRegistration() {
+        // yml에 있는 id, secret 가져오기
+        final String clientId = environment.getProperty(registration + "facebook.client-id");
+        final String clientSecret = environment.getProperty(registration + "facebook.client-secret");
+
+        return CommonOAuth2Provider
+                .FACEBOOK
+                .getBuilder("facebook")
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .build();
     }
 }
