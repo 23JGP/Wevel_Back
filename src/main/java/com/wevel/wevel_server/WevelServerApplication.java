16ef4c28b77bd54562867a2f31bd6e273fff88fa
mic050r <103114387+mic050r@users.noreply.github.com>
2024-01-21 00:47:09 +0900

    ✨[feat] : 프론트와 연결하기 위해 Cors 추가

--- src/main/java/com/wevel/wevel_server/WevelServerApplication.java
+++ src/main/java/com/wevel/wevel_server/WevelServerApplication.java
@@ -3,6 +3,9 @@ package com.wevel.wevel_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // spring security 로그인 화면 안뜨게
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
