551e88e0569be58f778f70624a09e0e27dda683c
mic050r <103114387+mic050r@users.noreply.github.com>
2024-01-23 13:53:18 +0900

    ✨[feat] : Deepl api를 사용하여 번역 기능 추가

---
+++ src/main/java/com/wevel/wevel_server/deepapi/DeepLConfig.java
@@ -0,0 +1,16 @@
+package com.wevel.wevel_server.deepapi;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+public class DeepLConfig {
+
+    @Value("${deepl.auth-key}")
+    private String authKey;
+
+    public String getAuthKey() {
+        return authKey;
+    }
+}
