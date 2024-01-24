e8165d5ddca2e5a6f3de834eaa2d67b3412221b4
Park Ye Eun <komgolo67@naver.com>
2024-01-24 11:23:08 +0900

    💩[feat] : 영수증 product 수정

--- src/main/java/com/wevel/wevel_server/receipt/service/ProductService.java
+++ src/main/java/com/wevel/wevel_server/receipt/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.wevel.wevel_server.receipt.service;
 
+import com.wevel.wevel_server.receipt.dto.ProductDTO;
 import com.wevel.wevel_server.receipt.entity.Product;
 import com.wevel.wevel_server.receipt.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,14 +22,19 @@ public class ProductService {
         return productRepository.save(product);
     }
 
-    public Product editProduct(Long productId, Product editedProduct) {
+    // ProductService
+    public ProductDTO editProduct(Long productId, ProductDTO editedProductDTO) {
         Product existingProduct = productRepository.findById(productId).orElse(null);
         if (existingProduct != null) {
-            existingProduct.setProductName(editedProduct.getProductName());
-            existingProduct.setQuantity(editedProduct.getQuantity());
-            existingProduct.setPrice(editedProduct.getPrice());
-            return productRepository.save(existingProduct);
+            existingProduct.setProductName(editedProductDTO.getProductName());
+            existingProduct.setQuantity(editedProductDTO.getQuantity());
+            existingProduct.setPrice(editedProductDTO.getPrice());
+            Product updatedProduct = productRepository.save(existingProduct);
+
+            // 수정된 Product 엔티티를 ProductDTO로 변환하여 반환
+            return new ProductDTO(updatedProduct.getProductName(), updatedProduct.getQuantity(), updatedProduct.getPrice());
         }
         return null;
     }
+
 }
